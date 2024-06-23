34814da2b67adc80056e9df42cba333f11a52a4d
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-06-23 17:16:04 +0530

    done till notification sent from notification channel

--- app/src/main/java/com/starkindustries/notification/MainActivity.kt
+++ app/src/main/java/com/starkindustries/notification/MainActivity.kt
@@ -3,19 +3,24 @@ import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationBuilderWithBuilderAccessor
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import com.starkindustries.notification.NotificationChannels.App
 import com.starkindustries.notification.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -43,6 +48,39 @@ class MainActivity : AppCompatActivity() {
                 manager.notify(101,notification)
             else
                 ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS),102)
+        }
+        binding.highButton.setOnClickListener()
+        {
+            var view = this.currentFocus
+            if(view!=null)
+            {
+                var manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(view.windowToken,0)
+            }
+            var notification = NotificationCompat.Builder(this,App().CHANNER_ID_1)
+            notification.setContentTitle(binding.title.text.toString().trim())
+            notification.setContentText(binding.content.text.toString().trim())
+            notification.setSmallIcon(R.drawable.batman)
+            notification.setPriority(NotificationCompat.PRIORITY_HIGH).build()
+            var manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.notify(1,notification.build())
+
+        }
+        binding.lowButton.setOnClickListener()
+        {
+            var view = this.currentFocus
+            if(view!=null)
+            {
+                var manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(view.windowToken,0)
+            }
+            var manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            var notification = NotificationCompat.Builder(this,App().CHANNEL_ID_2)
+            notification.setContentTitle(binding.title.text.toString().trim())
+            notification.setContentText(binding.content.text.toString().trim())
+            notification.setSmallIcon(R.drawable.batman)
+            notification.setPriority(NotificationCompat.PRIORITY_LOW).build()
+            manager.notify(2,notification.build())
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
